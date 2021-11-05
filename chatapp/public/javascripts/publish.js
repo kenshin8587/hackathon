f5b7b70fa584cdce16b5f05a789c7f21a79aa395
Ryo-Segawa <ryo.segawa1@gmail.com>
2021-11-05 16:00:04 +0900

    追加要望1,5を実装

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -7,7 +7,10 @@ function publish() {
     // 入力されたメッセージを取得
     const message = $('#message').val();
     // 投稿内容を送信
-    socket.emit('sendMessageEvent', message, userName);
+    // 空行や改行だけではないもの
+    if($.trim(message)){
+        socket.emit('sendMessageEvent', message, userName);
+    }
     console.log(userName);
     return false;
 }
@@ -16,7 +19,18 @@ function publish() {
 socket.on('receiveMessageEvent', function (data, userName) {
     // 「ユーザー名さん: コメント」を画面上に表示する
     $('#thread').prepend('<p>' + userName + 'さん:' + data + '</p>');
-
     // 投稿フィールドをリセット
     $('#message').val('');
 });
+
+// メッセージでエンターキーが押されたら投稿する
+$("#message").keypress(function(e) {
+    // エンターキーのみが押されたら投稿する
+    if (e.keyCode == 13 && e.shiftKey === false) {
+        publish();
+        return false;
+    }else{
+        // shiftとenterキーが両方押されたら、普通に改行をする
+    }
+});
+
