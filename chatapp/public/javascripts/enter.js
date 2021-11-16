e7feba660c22f169749c05c54fe4ad3cdbe7e1fc
Hayato-N24 <0409shido@gmail.com>
2021-11-16 21:01:24 +0900

    データベースの箱だけ完成

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -3,11 +3,27 @@
 // 入室メッセージをサーバに送信する
 // 入力されたユーザ名を取得する
 const userName = $('#userName').val();
-console.log('enter.js'+userName);
+//console.log('enter.js'+userName);
 // 入室メッセージイベントを送信する
 socket.emit('sendEnterEvent',userName);
 
+
 // サーバから受信した入室メッセージを画面上に表示する
 socket.on('receiveEnterEvent', function (data) {
+    console.log('db.js' + data);
     $('#thread').prepend('<p>' + data +'さんが入ってきたよ'+ '</p>');
+    console.log('db.js' + data);
+
+    const sqlite3 = require("sqlite3");
+    const db = new sqlite3.Database("./Users.db");
+    db.run("drop table if exists Users");  
+    console.log('db.js' + data);
+    db.run("create table if not exists Users(id integer primary key autoincrement,name, password)");
+    db.run("insert into Users(name,password) values(?,?)", "hoge", "pass2");
+    db.run("insert into Users(name,password) values(?,?)", data, "passhoge");
+    db.each("select * from Users", (err, row) => {
+        console.log(`${row.id}`, `${row.name}`, `${row.password}`);
+    });
+
+    db.close();
 });
--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -4,5 +4,6 @@ module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
     socket.on('sendEnterEvent', function (data) {
         socket.broadcast.emit('receiveEnterEvent', data);
+        
     });
 };
