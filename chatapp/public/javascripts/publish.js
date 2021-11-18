9e9454d8acb1755ccda3f1779d53c3c321b9c7d0
Ruri-Tanji <s1f101902219@iniad.org>
2021-11-18 18:59:59 +0900

    publish.db

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -6,10 +6,15 @@ function publish() {
     const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
+
+    //投稿時間を取得
+    var dt = new Date();
+    var formatted = dt.toLocaleString({ timeZone: 'Asia/Tokyo' });
+    
     // 投稿内容を送信
     // 空行や改行だけではないもの
     if($.trim(message)){
-        socket.emit('sendMessageEvent', message, userName);
+        socket.emit('sendMessageEvent', message, userName, formatted);
         // 投稿フィールドをリセット
         $('#message').val('');
     }
@@ -19,19 +24,15 @@ function publish() {
 
 // サーバから受信した投稿メッセージを画面上に表示する
 //他人用
-socket.on('receiveMessageEvent', function (data, userName) {
+socket.on('receiveMessageEvent', function (data, userName, formatted) {
     // 「ユーザー名さん: コメント」を画面上に表示する
-    var dt = new Date();
-    var formatted = dt.toLocaleString({ timeZone: 'Asia/Tokyo' });
     data = data.replace("\n", "<br>");
     $('#thread').prepend('<div class="incoing_msg"><div class="received_msg"><div class="received_with_msg"><p>'+data+'</p><span class="time_date">'+formatted+'</span></div></div></div>');
 });
 
 //自分用
-socket.on('selfReceiveMessageEvent', function (data, userName) {
+socket.on('selfReceiveMessageEvent', function (data, userName, formatted) {
     // 「ユーザー名さん: コメント」を画面上に表示する
-    var dt = new Date();
-    var formatted = dt.toLocaleString({ timeZone: 'Asia/Tokyo' });
     data = data.replace("\n", "<br>");
     $('#thread').prepend('<div class="outgoing_msg">'+'<button onclick="trash()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button>'+'<div class="sent_msg"><p>'+data+'</p><span class="time_date">'+formatted+'</span></div></div>');
 });
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -4,11 +4,34 @@ const { use } = require("../routes");
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
-    socket.on('sendMessageEvent', function (data, userName) {
+    socket.on('sendMessageEvent', function (data, userName, formatted) {
         //自分宛
-        socket.emit('selfReceiveMessageEvent', data, userName);
+        socket.emit('selfReceiveMessageEvent', data, userName, formatted);
         //他人宛
-        socket.broadcast.emit('receiveMessageEvent', data, userName);
-        console.log(userName + 'さんから' + data + 'というコメントを受け取りました');
+        socket.broadcast.emit('receiveMessageEvent', data, userName, formatted);
+        console.log(userName + 'さんから' + data + 'というコメントを受け取りました' + formatted);
+
+        //sqlite3を使えるようにしている
+        const sqlite3 = require("sqlite3");
+
+        //データベースを操作するためのインスタンス的なやつを生成している?
+        //引数は使用するデータベースファイルだと思う。ここで使用されているのはhackathon/chatapp/Publish.db
+        const db = new sqlite3.Database("./Publish.db");
+
+        //データを保存するためコメントアウト。テーブルを消したければコメントアウト外す。
+        //db.run("drop table if exists Publish");
+
+        //テーブルの作成
+        db.run("create table if not exists Publish(id integer primary key autoincrement, name, message, time)");
+
+        //入力されたユーザーネーム、メッセージ、時間をデータベースに保存。
+        db.run("insert into Publish(name, message, time) values(?,?, ?)", userName, data, formatted);
+
+        //データベースに保存されているデータの出力
+        db.each("select * from Publish", (err, row) => {
+            console.log(`${row.id}`, `${row.name}`, `${row.message}`, `${row.time}`);
+        });
+
+        db.close();
     });
 };
